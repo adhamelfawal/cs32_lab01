e131f01661a17ecf8002436b2b995bfcc3bad635
adhamelfawal <73265789+adhamelfawal@users.noreply.github.com>
2021-04-13 18:45:02 -0400

    Add files via upload

--- demogData.cpp
+++ demogData.cpp
@@ -7,14 +7,15 @@ std::ostream& operator<<(std::ostream &out, const demogData &DD) {
     out << " total population: " << DD.totalPopulation2014;
     //consider re-writing with getters....
     out << "\nPopulation info: \n(\% over 65): " << DD.popOver65;
-    out <<  " Count: " << (DD.popOver65 * DD.totalPopulation2014 * .01);
+    out <<  " Count: " << DD.getpopOver65Count();
     out << "\n(\% under 18): " << DD.popUnder18;
-    out <<  " Count: " << (DD.popUnder18 * DD.totalPopulation2014 * .01);
+    out <<  " Count: " << DD.getpopUnder18Count();
     out << "\n(\% under 5): " << DD.popUnder5;
-    out <<  " Count: " << (DD.popUnder5 * DD.totalPopulation2014 * .01);
+    out <<  " Count: " << DD.getpopUnder5Count();
     out << "\nEducation info: \n(\% Bachelor degree or more): " << DD.undergrad;
-    out << " Count: "<< (DD.undergrad * DD.totalPopulation2014 * .01);
+    out << " Count: "<< DD.getBAupCount();
     out << "\n(\% high school or more): "<< DD.highschool;
-    out <<  " Count: " << (DD.highschool * DD.totalPopulation2014 * .01);
+    out <<  " Count: " << DD.getHSupCount();
     return out;
 }
+
